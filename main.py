d45b88cc54129771eb755ee3eedac573027703fb
Bokagyi <85953820+Bokagyi@users.noreply.github.com>
2025-04-11 22:26:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENA_API_KEY")
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
